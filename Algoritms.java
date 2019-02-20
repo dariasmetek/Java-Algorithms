d40b10d1dc57d0f3b6ff97387d3ac1d2c6938410
dariasmetek <47626013+dariasmetek@users.noreply.github.com>
2019-02-20 11:01:38 +0100

    Add files via upload

---
+++ Algoritms.java
@@ -0,0 +1,27 @@
+package Z1;
+
+
+
+    public class Algoritms{
+        //algorytm potegowania
+        public int power(int x, int y){
+
+         //2
+            int result = 1;
+         //3
+            for(int i = 1; i <=y; i++ ) {
+                result = result * x;
+            }
+            return result;
+
+
+        }
+        //algorytm silnia z n
+        public int factorial(int n){
+            //2
+
+            //3
+            return
+        }
+
+    }
---
+++ Main.java
@@ -0,0 +1,9 @@
+package Z1;
+
+public class Main {
+    public static void main(String[] args) {
+        Algoritms a = new Algoritms();
+        System.out.println("Wynik potegowania: " + a.power(3, 4));
+
+    }
+}
