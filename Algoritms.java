08cbfa8495f540167fcb4518ec5b59a7b213671a
dariasmetek <47626013+dariasmetek@users.noreply.github.com>
2019-02-20 11:45:41 +0100

    Add files via upload

--- Algoritms.java
+++ Algoritms.java
@@ -19,9 +19,28 @@ package Z1;
         //algorytm silnia z n
         public int factorial(int n){
             //2
-
+            int result = 1;
             //3
-            return
+            for(int i = n; i > 1; i--){
+                result = result * i;
+            }
+            return result;
         }
+        //algorytm ciagu geometrycznego
+        public int geoSequence(int a0, int q, int n){
+            int sum = 0;
+            for (int i = 0; i <= n-1; i++){
+                sum = sum + (a0 * power(q, i));
 
+            }
+            return sum;
+        }
+        //algorytm ciagu arytmetycznego
+        public int algSequence(int a0, int r, int n){
+            int sum = 0;
+            for(int i = 0; i < n-1; i++){
+                sum = sum + (a0 + i*r);
+            }
+            return sum;
+        }
     }
--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Algoritms a = new Algoritms();
         System.out.println("Wynik potegowania: " + a.power(3, 4));
-
+        System.out.println("Wynik silni: " + a.factorial(4));
+        System.out.println("Suma cg: " + a.geoSequence(2,2,3));
+        System.out.println("Suma ca: " + a.algSequence(2,2,4));
     }
 }
